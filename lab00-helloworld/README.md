d1d12b26db378e98b5c5dde08c42aefb9fa6dfe2
Tran Minh Quan <quantm@unist.ac.kr>
2014-09-12 11:49:36 +0900

    Update

---
+++ lab00-helloworld/README.md
@@ -0,0 +1,7 @@
+Lab00 Instruction
+=======
+```bash
+nvcc -arch=sm_35 helloworld.cu -o helloworld
+```
+
+
--- lab00-helloworld/helloworld.cu
+++ lab00-helloworld/helloworld.cu
@@ -1,30 +1,34 @@
 #include <cuda.h>
 #include <stdio.h>
 
+///A device kernel function, just print out "Hello World"
 __global__
-void helloword()
+void helloworld()
 {
-	printf("Hello World\n")
+	printf("Hello World\n");
 }
 
 int main(int argc, char** argv)
 {
-	//Launch 1 block , each block has 1 thread 
+	///Launch 1 block , each block has 1 thread 
 	printf("Launch 1 block , each block has 1 thread \n");
-	helloword<<<  1, 1 >>>();
+	helloworld<<<  1, 1 >>>();
 	printf("\n");
 	
-	//Launch 1 block , each block has 3 threads
+	///Launch 1 block , each block has 3 threads
 	// printf("Launch 1 block , each block has 3 threads\n");
-	// helloword<<<  /*TODO*/, 3 >>>();
+	// helloworld<<<  /*TODO*/, 3 >>>();
+	// printf("\n");
 	
-	//Launch 2 blocks, each block has 1 thread
+	///Launch 2 blocks, each block has 1 thread
 	// printf("Launch 2 blocks, each block has 1 thread \n");
-	// helloword<<<  /*TODO*/, 3 >>>();
-		
-	//Launch 2 blocks, each block has 3 threads
+	// helloworld<<<  /*TODO*/, 3 >>>();
+	// printf("\n");
+	
+	///Launch 2 blocks, each block has 3 threads
 	// printf("Launch 2 blocks, each block has 3 threads\n");
-	// helloword<<<  /*TODO*/, /*TODO*/ >>>();
+	// helloworld<<<  /*TODO*/, /*TODO*/ >>>();
+	// printf("\n");
 	
 	return 0;
 }
