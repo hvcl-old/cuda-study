77cfd974341e830a4a59f9a1c6a41e6ddafef579
Tran Minh Quan <quantm@unist.ac.kr>
2014-09-12 12:43:46 +0900

    Update

--- lab00-helloworld/README.md
+++ lab00-helloworld/README.md
@@ -1,7 +1,7 @@
 Lab00 Instruction
 =======
 ```bash
-nvcc -arch=sm_35 helloworld.cu -o helloworld
+nvcc helloworld.cu -o helloworld
 ```
 * cudaSetDevice()
 * numBlocksPerGrid
--- lab00-helloworld/helloworld.cu
+++ lab00-helloworld/helloworld.cu
@@ -14,22 +14,22 @@ int main(int argc, char** argv)
 	cudaSetDevice(0);
 	
 	///Launch 1 block , each block has 1 thread 
-	printf("Launch 1 block , each block has 1 thread \n");
+	printf("Launch 1 block , each block has 1 thread");
 	helloworld<<<  1, 1 >>>();
 	printf("\n");
 	
 	///Launch 1 block , each block has 3 threads
-	// printf("Launch 1 block , each block has 3 threads\n");
+	// printf("Launch 1 block , each block has 3 threads");
 	// helloworld<<<  1, /*TODO*/ >>>();
 	// printf("\n");
 	
 	///Launch 2 blocks, each block has 1 thread
-	// printf("Launch 2 blocks, each block has 1 thread \n");
+	// printf("Launch 2 blocks, each block has 1 thread");
 	// helloworld<<<  /*TODO*/, 1 >>>();
 	// printf("\n");
 	
 	///Launch 2 blocks, each block has 3 threads
-	// printf("Launch 2 blocks, each block has 3 threads\n");
+	// printf("Launch 2 blocks, each block has 3 threads");
 	// helloworld<<<  /*TODO*/, /*TODO*/ >>>();
 	// printf("\n");
 	
